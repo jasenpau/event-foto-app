9ad2367cf2d5326f6084440be403f983f826e274
jasenpau <paulius.jasenas@ktu.edu>
2025-03-26 00:07:55 +0200

    Event list improvements pt.2

--- EventFoto/EventFoto.Data/DTOs/EventListDto.cs
+++ EventFoto/EventFoto.Data/DTOs/EventListDto.cs
@@ -7,6 +7,7 @@ public record EventListDto
     public int Id { get; init; }
     public string Name { get; init; }
     public DateTime StartDate { get; init; }
+    public DateTime? EndDate { get; init; }
     public bool IsArchived { get; init; }
 
     public static EventListDto FromEvent(Event eventData)
@@ -16,6 +17,7 @@ public record EventListDto
             Id = eventData.Id,
             Name = eventData.Name,
             StartDate = eventData.StartDate,
+            EndDate = eventData.EndDate,
             IsArchived = eventData.IsArchived
         };
     }
