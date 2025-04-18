e37a659435415b46aecba2fecd9ad2f8130f84fe
Paulius <paulius.jasenas@ktu.edu>
2025-04-18 13:29:00 +0300

    Add API health check endpoint

---
+++ EventFoto/EventFoto.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventFoto.API.Controllers;
+
+// API route for checking the health status of API service.
+// Returns 200 OK, when API is up.
+
+[ApiController]
+[Route("api/[controller]")]
+[AllowAnonymous]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+        return Ok("OK");
+    }
+}
