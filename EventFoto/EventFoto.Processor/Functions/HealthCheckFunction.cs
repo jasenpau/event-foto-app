ac4be465b413489570f9c751cfa35aade7551738
Paulius <paulius.jasenas@ktu.edu>
2025-04-18 21:39:27 +0300

    Add Health check function

---
+++ EventFoto/EventFoto.Processor/Functions/HealthCheckFunction.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace EventFoto.Processor.Functions;
+
+public class HealthCheckFunction
+{
+    [Function("HealthCheck")]
+    public static HttpResponseData Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "health")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.WriteString("Healthy");
+        return response;
+    }
+}
